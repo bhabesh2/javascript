7fd7a9d7addaf670b49fdc755941a69f323ad333
bhabesh2 <jsarma058@gmail.com>
2023-06-11 21:47:23 +0530

    Object added

---
+++ lecture4/index.js
@@ -0,0 +1,31 @@
+// Objects here:
+
+/**
+ * In JavaScript everything is an Object.
+ * Objects are a complex data type in JavaScript.
+ * Object is a key-value or name-value pair.
+ * Object can store any kind of data types as key-value pair.
+ * Objects consist of properties and can have methods.
+ * Object is declared using curly braces {}.
+ * Properties can be accessed using dot notation or bracket notation.
+ * Objects can be created using object literal syntax, constructors, or classes.
+ */
+
+/*
+let arr = ['name','Rohan',2,3,4] // check the type of this array.
+console.log(typeof arr) // Object.
+*/
+
+/*
+
+function add(a,b){
+    return a+b;
+}
+add(2,3);
+
+console.log(typeof add); // function . remember functions are also objects.
+
+Functions in JavaScript are considered "first-class citizens," meaning they can be assigned to variables, passed as arguments, and returned as values.
+*/
+
+
